20e77c2df6fa97a14da0d36b6b72edc5ed47bf4a
Gabriel Gadotti <gabriel-gadotti@hotmail.com>
2021-05-04 17:29:31 -0300

    Basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,12 @@
+function Footer() {
+    return (
+        <footer className="footer mt-auto py-3 bg-dark">
+            <div className="container">
+                <p className="text-light">App desenvolvido por <a href="https://github.com/gabrielgadottidecampos" target="_blank" rel="noreferrer">Gabriel Gadotti</a></p>
+                <p className="text-light"><small><strong>Semana Spring React</strong><br /></small></p>
+            </div>
+        </footer>
+    );
+}
+
+export default Footer;
